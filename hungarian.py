4b0830882df164ab28dd46d8088b0d8280135a19
sumuk <sumukbadam@gmail.com>
2022-05-16 15:48:32 -0700

    loss matching using cost from bbox

--- hungarian.py
+++ hungarian.py
@@ -3,10 +3,14 @@ import torch
 import scipy
 
 class HungarianMatcher():
-    def __init__(self):
-        pass
+    
+    def __init__(self,num_class=20):
+        self.bound_loss = torch.nn.MSELoss()
+        self.class_loss = torch.nn.CrossEntropyLoss()
+        self.num_class = num_class
+        
     @torch.no_grad()
-    def forward(self,targets,output):
+    def get_index(self,targets,output):
         '''
         targets dic of class and bbox
             class_size [num_class] only one bbox for img
@@ -21,5 +25,16 @@ class HungarianMatcher():
         target_bbox = targets['bbox']
         target_class = targets['class']
 
-        dist = torch.cdist(output_bbox, target_bbox.unsqueeze(1), p=1)
+        dist = torch.cdist(output_bbox, target_bbox.unsqueeze(1).to(torch.float), p=1)
+        return torch.argmax(dist,axis=1).squeeze(1)
+    
+    def loss(self,target_dic,output_dic):
+        batch_size = output_dic['bbox'].size(0)
+        num_queries = output_dic['bbox'].size(1)
+        idx = self.get_index(target_dic,output_dic)
+        bound_loss = self.bound_loss(output_dic['bbox'][np.arange(batch_size),idx],target_dic['bbox'])
+        class_label = torch.ones(batch_size,num_queries)
+        class_label[idx] = 0
+        class_loss = self.class_loss(output_dic['class'].permute(0,2,1),class_label.to(torch.long))
+        return class_loss + bound_loss
         
